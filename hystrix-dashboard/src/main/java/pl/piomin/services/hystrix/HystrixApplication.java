e5b8eae936a566a3a052aac37019762407317155
Piotr Mińkowski <piotr_minkowski@wp.pl>
2017-12-12 23:43:45 +0100

    enabling stream for turbine

--- hystrix-dashboard/src/main/java/pl/piomin/services/hystrix/HystrixApplication.java
+++ hystrix-dashboard/src/main/java/pl/piomin/services/hystrix/HystrixApplication.java
@@ -3,11 +3,11 @@ package pl.piomin.services.hystrix;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableTurbine
+@EnableTurbineStream
 public class HystrixApplication {
 	
 	public static void main(String[] args) {
