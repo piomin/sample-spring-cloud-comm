1ce5b38c556f7bf86c8794d4d89706d3a101aa6a
piotr.minkowski <piotr.minkowski@gmail.com>
2017-12-12 07:42:54 +0100

    remove pingrule

--- customer-service/src/main/java/pl/piomin/services/customer/config/RibbonConfiguration.java
+++ customer-service/src/main/java/pl/piomin/services/customer/config/RibbonConfiguration.java
@@ -3,9 +3,7 @@ package pl.piomin.services.customer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.WeightedResponseTimeRule;
 
 @Configuration
@@ -14,11 +12,6 @@ public class RibbonConfiguration {
 	@Bean
 	public IRule ribbonRule() {
 		return new WeightedResponseTimeRule();
-	}
-	
-	@Bean
-	public IPing ribbonPing() {
-		return new PingUrl();
 	}
 	
 }
