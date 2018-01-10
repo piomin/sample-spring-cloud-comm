baf9ee70ba2c8f30b9b875d535e271507f89f805
piotr.minkowski <piotr.minkowski@gmail.com>
2018-01-10 15:51:18 +0100

    port changes

--- gateway-service/src/test/java/pl/piomin/services/gateway/GatewayControllerTest.java
+++ gateway-service/src/test/java/pl/piomin/services/gateway/GatewayControllerTest.java
@@ -27,9 +27,9 @@ public class GatewayControllerTest {
 			Order order = new Order();
 			order.setCustomerId((long) r.nextInt(3) + 1);
 			order.setProductIds(Arrays.asList(new Long[] { (long) r.nextInt(10) + 1, (long) r.nextInt(10) + 1 }));
-			order = template.postForObject("http://localhost:8085/api/order", order, Order.class);
+			order = template.postForObject("http://localhost:8080/api/order", order, Order.class);
 			if (order.getStatus() != OrderStatus.REJECTED) {
-				template.put("http://localhost:8085/api/order/{id}", null, order.getId());
+				template.put("http://localhost:8080/api/order/{id}", null, order.getId());
 			}
 		} catch (Exception e) {
 
